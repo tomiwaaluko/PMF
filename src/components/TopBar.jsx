bab8d1527df732fb5309cc7fc2bf03db651914fd
Luis Alban <alalban@hotmail.com>
2025-12-15 14:06:47 -0500

    address on nav bar takes you to map

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,7 +4,14 @@ function TopBar() {
       <a className="top-link" href="tel:4072179122">
         Call: (407) 217-9122
       </a>
-      <span>3098 W Lake Mary Blvd 2nd Floor Lake Mary, FL 32746</span>
+      <a
+        className="top-link"
+        href="https://www.google.com/maps/search/?api=1&query=3098+W+Lake+Mary+Blvd+2nd+Floor+Lake+Mary,+FL+32746"
+        target="_blank"
+        rel="noreferrer"
+      >
+        3098 W Lake Mary Blvd 2nd Floor Lake Mary, FL 32746
+      </a>
       <a href="https://yourkey.mymortgage-online.com/loan-app/?siteId=5842573753&lar=ahall&workFlowId=99040">
         <button className="apply-btn">Apply Online</button>
       </a>
